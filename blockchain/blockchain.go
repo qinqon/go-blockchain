4c937af3617d252d19ad994459a4fd63986e22bf
Felix Enrique Llorente Pastora <felix.enrique.llorente.pastora@ericsson.com>
2017-10-14 08:55:24 +0200

    Add proof of work
    Do a tipical blockchain compare with latest part of the hashed block

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -5,17 +5,19 @@ import "log"
 import "encoding/json"
 import "crypto/sha256"
 import "encoding/hex"
+import "fmt"
 
 type Blockchain struct{
    chain []*Block
    currentTransactions []Transaction
 }
-func NewBlockchain() *Blockchain {
+func New() *Blockchain {
    bc := Blockchain{}
    return &bc
 }
 
 func Hash(block *Block) string{
+   //TODO: Sort dictionary
    marshalledBlock, err := json.Marshal(*block)
    if err != nil {
       log.Fatal(err)
@@ -25,7 +27,22 @@ func Hash(block *Block) string{
    return hexBlock
 }
 
-func (b *Blockchain) NewBlock(proof int, previousHash int) *Block{
+func (b *Blockchain) ValidProof(lastProof, proof int) bool {
+   guess := []byte(fmt.Sprintf("%i%i", lastProof, proof))
+   guessHash := sha256.Sum256(guess)
+   //FIXME: Don't convert to string, compare the slice
+   return string(guessHash[len(guessHash)-4]) == "0000"
+}
+
+
+func (b *Blockchain) ProofOfWork(lastProof int) int {
+   proof := 0
+   for ; b.ValidProof(proof, lastProof); proof++ {
+   }
+   return proof
+}
+
+func (b *Blockchain) NewBlock(proof int, previousHash string) *Block{
    block := Block{
       Index: len(b.chain) + 1,
       Timestamp: time.Time{},
--- main.go
+++ main.go
@@ -3,8 +3,5 @@ package main
 import ("github.com/qinqon/go-blockchain/blockchain")
 
 func main() {
-   //bc := blockchain.NewBlockchain()
-   //bc.NewTransaction("Foo", "bar", 666)
-   
-   print(blockchain.Hash(&block))
+   blockchain.StartWebServer()
 }
