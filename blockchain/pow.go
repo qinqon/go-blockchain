c47c77e04ab5dca06667118f0b02722cc7cea7b7
Felix Enrique Llorente Pastora <felix.enrique.llorente.pastora@ericsson.com>
2017-11-07 15:05:02 +0100

    Use the gofmt -s formatter

--- blockchain/pow.go
+++ blockchain/pow.go
@@ -10,7 +10,7 @@ func ValidProof(lastProof, proof int) error {
 	guess := fmt.Sprintf("%v%v", lastProof, proof)
 	guessHash := sha256.Sum256([]byte(guess))
 	guessHashEncoded := hex.EncodeToString(guessHash[:])
-	if !(guessHashEncoded[len(guessHashEncoded)-4:len(guessHashEncoded)] == "0000") {
+	if !(guessHashEncoded[len(guessHashEncoded)-4:] == "0000") {
 		return fmt.Errorf("Hash '%v' of '%v' not ending with 4 zeroes", guessHashEncoded, guess)
 	}
 	return nil
